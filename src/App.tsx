a57af674ab46cc1796125f11685dc78b227c159e
Alief Ardiansyah <aliefardiansyah12@gmail.com>
2023-10-01 23:18:18 +0700

    Upload Semua file ke github

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import Skills from "./components/Skills";
+import About from "./components/About";
+import Projects from "./components/Projects";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <About />
+      <Projects />
+    </>
+  );
+};
+export default App;
---
+++ src/components/SkillsCard.tsx
@@ -0,0 +1,15 @@
+type SectionCardProps = {
+  icon: JSX.Element;
+  title: string;
+  text: string;
+};
+const SectionCard: React.FC<SectionCardProps> = ({ icon, title, text }) => {
+  return (
+    <article>
+      <span className="h-16 w-16">{icon}</span>
+      <h4 className="mt-6 font-bold">{title}</h4>
+      <p className="mt-2 text-slate-500">{text}</p>
+    </article>
+  );
+};
+export default SectionCard;
